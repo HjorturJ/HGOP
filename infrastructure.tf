1e359efa788a58f699f91611333bb40ee72a5f6e
Guðrún Margrét Ívansdóttir <gudruni17@ru.is>
2019-11-28 14:17:47 +0000

    Commented the infrastructure.tf file and initialize_game_api_instance.sh

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,10 +1,14 @@
-# TODO Comment 2-3 sentences.
+# Configure the AWS Provider
+# The provider needs to be configured with the proper credentials before it can be used
+# Our credentials are stored in the credentials file
 provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
   region                  = "us-east-1"
 }
 
-# TODO Comment 2-3 sentences.
+# We are here creating security group
+# and allowing access to port 3000 from anywhere 
+# when using a TCP connection
 resource "aws_security_group" "game_security_group" {
   name = "GameSecurityGroup"
 
@@ -30,7 +34,8 @@ resource "aws_security_group" "game_security_group" {
   }
 }
 
-# TODO Comment 2-3 sentences.
+# Actually creating the aws instances of our server
+# with the proper settings
 resource "aws_instance" "game_server" {
   ami                    = "ami-0ac019f4fcb7cb7e6"
   instance_type          = "t2.micro"
@@ -40,7 +45,8 @@ resource "aws_instance" "game_server" {
     Name = "GameServer"
   }
 
-  # TODO Comment 1-2 sentences.
+  # Copying initialize script into our server through ssh
+  # so it can be used there
   provisioner "file" {
     source      = "scripts/initialize_game_api_instance.sh"
     destination = "/home/ubuntu/initialize_game_api_instance.sh"
@@ -53,7 +59,8 @@ resource "aws_instance" "game_server" {
     }
   }
 
-  # TODO Comment 1-2 sentences.
+  # Copying docker-compose into our server through ssh
+  # so it can be used there
   provisioner "file" {
     source      = "docker-compose.yml"
     destination = "/home/ubuntu/docker-compose.yml"
@@ -71,7 +78,9 @@ resource "aws_instance" "game_server" {
   # Since it can take time for the SSH agent on machine to start up we let Terraform
   # handle the retry logic, it will try to connect to the agent until it is available
   # that way we know the instance is available through SSH after Terraform finishes.
-  # TODO Comment 1-2 sentences.
+
+  # Executing the chmod +x in our server on the local 
+  # initialize script to make it executable
   provisioner "remote-exec" {
     inline = [
       "chmod +x /home/ubuntu/initialize_game_api_instance.sh",
@@ -86,7 +95,7 @@ resource "aws_instance" "game_server" {
   }
 }
 
-# TODO Comment 1-2 sentences.
+# Returning our server public ip from this script
 output "public_ip" {
   value = aws_instance.game_server.public_ip
 }
