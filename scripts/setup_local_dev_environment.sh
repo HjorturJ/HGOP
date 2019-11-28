14cca9475bfc9aa89f5325e120e942278d447490
hjortur <hjorturjohann94@gmail.com>
2019-11-28 15:21:19 +0000

    Remade env script to use brew, makes it much simpler and easier to understand

--- scripts/setup_local_dev_environment.sh
+++ scripts/setup_local_dev_environment.sh
@@ -7,104 +7,98 @@ exec > >(tee log_file.txt)
 start_datetime=$(date)
 
 # Display date and time of script start.
-echo "Dependency script started on: $start_datetime..."
+echo "Dev environment script started on: $start_datetime..."
 
 # Prompts the user with info on what the script does and what operating system it is running on 
-echo "Welcome $USER! This script installs required programs/dependencies
-(Git and NodeJS) on your $OSTYPE machine! 
-Note that Brew will be installed if you are running on macOs."
+echo "Welcome $USER! This script installs required programs/dependencies (Git, CURL, Brew, PIP3, NodeJS, NPM, AWS CLI and Terraform) on your $OSTYPE machine!"
 
-    # Makes sure to check if the operating system is running on macOs and then using 
-    # Homebrew to install following required programs/dependencies
-    if [[ $OSTYPE == darwin* ]]; then
+# Run this command to make sure everything is up to date so download and installations have a higher chance of running successfully.
+sudo apt-get update
+
+# Check OSTYPE since we need to install git before we install homebrew on linux and linux doesnt have curl default installed.
+if [[ $OSTYPE == darwin* ]]; then
         # Check if Homwbrew is already installed. Install it if not. 
         if test ! $(which brew); then
-            echo "Installing homebrew:"
+            # Display that we are now starting to install homebrew.
+            echo "Installing homebrew..."
             ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        else 
-            # Update Homebre
-            echo "Updating homebrew:"
-            brew update
         fi
+
         # Check if git is already installed. Install it if not. 
         if test ! $(which git); then
-            echo "Installing git:"
+            # Display that we are now starting to install git.
+            echo "Installing git..."
             brew install git 
-        else 
-            # Upgrate git 
-            echo "Upgrate git: "
-            brew upgrade git 
         fi
-        # Check if NodeJs and npm is already installed. Install it if not. 
-        if test ! $(which node); then
-            echo "Downloading and Installing NodeJs:"
-            brew install node
-        else 
-            # Upgrate Node
-            echo "Upgrate node:"
-            brew upgrade node
-        fi
+else
+    if test ! $(which git); then
+        # Display that we are now starting to install git.
+        echo "Installing git..."
 
-        echo "Downloading AWS CLI v1"
-
-        # Install the AWS CLI version 1 without Sudo and pip 
-        curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-        unzip awscli-bundle.zip
-        ./awscli-bundle/install -b ~/bin/aws
-
-         # Terraform installed with brew 
-        echo "Installing Terraform"
-        brew install terraform
-
-    else 
-        #Display that we are now starting to install git.
-        echo "Installing git:"
-
-        #Run this command to make sure everything is up to date so download and installations have a higher chance of running successfully.
-        sudo apt-get update
-
-        #Install git.
+        # Install git.
         sudo apt-get install git
-
-        #Display that we are now starting to install curl.
-        echo "Installing CURL:"
-
-        #Installing CURL to download NodeJs
-        sudo apt-get install curl
-        
-        #Display that we are about to install NodeJs
-        echo "Downloading and installing NodeJs:"
-
-        #GET NodeJs setup.
-        curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-
-        #Install NodeJs.
-        sudo apt-get install -y nodejs
-
-        #Say that we are about to install pip3 and aws cli
-        echo "Downloading pip and then AWS CLI v1"
-
-        #Get pip install
-        curl -O https://bootstrap.pypa.io/get-pip.py
-
-        #Install pip
-        python3 get-pip.py --user
-
-        #Install AWC CLI with pip
-        pip3 install awscli --upgrade --user
-
     fi
+
+    if test ! $(which curl); then
+        # Display that we are now starting to install curl.
+        echo "Installing CURL..."
+
+        # Installing CURL to download packages for other programs
+        sudo apt-get install curl
+    fi
+
+    if test ! $(which brew); then
+        # Display that we are now starting to install homebrew.
+        echo "Installing homebrew..."
+        
+        # Download and install homebrew from this path
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+        # These test commands ensure that the homebrew PATH is correctly setup to use homebrew using command line.
+        test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+        test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+        test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+        echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+    fi
+fi
+
+ # Check if NodeJs and npm is already installed. Install it if not. 
+if test ! $(which node); then
+    echo "Downloading and Installing NodeJs..."
+    brew install node
+else 
+    # Upgrade Node
+    echo "Upgrade node..."
+    brew upgrade node
+fi
+
+echo "Downloading AWS CLI v1"
+
+# Install the AWS CLI version 1 without Sudo and pip 
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+
+# Unzip the file we downloaded
+unzip awscli-bundle.zip
+
+# Intalling aws cli
+./awscli-bundle/install -b ~/bin/aws
+
+# Removing files that we downloaded after installing
+rm -r awscli-bundle.zip
+rm -r awscli-bundle
+
+# Terraform installed with brew 
+echo "Installing Terraform"
+brew install terraform
 
 # Storing date time of script completion.
 finished_datetime=$(date)
 
 # Display date time of the script completion.
-echo -e "Dependency script completed on: $finished_datetime \nVersions:"
+echo -e "Dev environment script completed on: $finished_datetime \nVersions..."
 
 # Display install version of brew for macOS
-if [[ $OSTYPE == darwin* ]]; then
-    brew --version
-fi
+brew --version
 
 # Display installed version of git.
 git --version
@@ -116,6 +110,6 @@ node --version
 echo -ne "NPM version "
 npm --version
 
-pip3 --version
-
 aws --version
+
+terraform --version
