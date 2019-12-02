75762c456847739fd08da828136d70b49fbb92aa
Guðrún Margrét Ívansdóttir <gudruni17@ru.is>
2019-12-02 20:04:40 +0000

    Test for getCardValue created, should be undefined at the start of the game, then 9

--- game_api/lucky21.unit-test.js
+++ game_api/lucky21.unit-test.js
@@ -47,3 +47,29 @@ test('guess21OrUnder should draw the next card', () => {
   expect(game.state.cards.length).toEqual(3);
   expect(game.state.cards[2]).toEqual('01D');
 });
+
+test('getCardValue should be undefined at the start of the game, and 9 after the first draw', () => {
+
+  // Arrange 
+  let deck = deckConstructor();
+  deck = [
+      '05C', '01D', '09S', '10H', 
+  ];
+
+  let dealer = dealerConstructor();
+  // Override the shuffle to do nothing.
+  dealer.shuffle = (deck) => {};
+
+  // Inject our dependencies
+  let game = lucky21Constructor(deck, dealer);
+
+  // Act
+  let cardValue = game.getCardValue(game);
+
+  // Assert
+  expect(cardValue).toEqual(undefined);
+  
+  //game.guess21OrUnder(game);
+  //cardValue = game.getCardValue(game);
+  //expect(cardValue).toEqual(9);
+});
