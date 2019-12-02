b1d68324ffaa06b4a80c0836de6d699a5ebc1d40
hjortur <hjorturjohann94@gmail.com>
2019-12-02 20:03:58 +0000

    Test for getCardsValue started, start of game and after 1 draw done. Missing expecting higher number than 20, since I havent started thinking of how to convert the card strings to ints

--- game_api/lucky21.unit-test.js
+++ game_api/lucky21.unit-test.js
@@ -30,7 +30,7 @@ test('guess21OrUnder should draw the next card', () => {
   // Arrange
   let deck = deckConstructor();
   deck = [
-      '05C', '01D', '09S', '10H', 
+      '05C', '01D', '09S', '10H'
   ];
 
   let dealer = dealerConstructor();
@@ -46,4 +46,30 @@ test('guess21OrUnder should draw the next card', () => {
   // Assert
   expect(game.state.cards.length).toEqual(3);
   expect(game.state.cards[2]).toEqual('01D');
+});
+
+
+
+
+test('getCardsValue should give us 15 total at the start of the game, and 19 after one draw', () => {
+  // Arrange
+  let deck = deckConstructor();
+  deck = [
+    '07C', '08D', '04S', '10H'
+  ];
+
+  let dealer = dealerConstructor();
+  // Override the shuffle to do nothing.
+  dealer.shuffle = (deck) => {};
+
+  // Act
+  let game = lucky21Constructor(deck, dealer);
+  let totalCardValue = game.getCardValue(game);
+
+  // Assert
+  expect(totalCardValue).toEqual(15);
+  // game.guess21OrUnder(game);
+  // Debateable if guess21OrUnder puts new card into the cards array right away (Probably though)
+  // totalCardValue = game.getCardValue(game);
+  // expect(totalCardValue).toEqual(19);
 });
