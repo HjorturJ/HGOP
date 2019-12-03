37f53f9cd8d8f437e4d70ea25241e78520aa78cf
hjortur <hjorturjohann94@gmail.com>
2019-12-03 14:16:21 +0000

    Changing file path to game_api instead of item_repo for jenkins

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,6 +3,6 @@
 #Get first argument of executable!
 GIT_COMMIT=$1
 
-docker build -t hjorturj/hgop:$GIT_COMMIT item_repository/
+docker build -t hjorturj/hgop:$GIT_COMMIT game_api/
 
 # TODO exit on error if any command fails
