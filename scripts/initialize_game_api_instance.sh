7bcafcfe359936c447347d17214b5656ed2cb675
hjortur <hjorturjohann94@gmail.com>
2019-12-03 18:43:29 +0000

    Apparently changing stable to test in the docker repo link can fix this problem, seems risky to change to a test build instead of a stable one but lets follow some random guy online and see what happens

--- scripts/initialize_game_api_instance.sh
+++ scripts/initialize_game_api_instance.sh
@@ -12,7 +12,9 @@ sudo apt-get install -y \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    test"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker ubuntu
