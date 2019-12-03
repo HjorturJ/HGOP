6df0a747b5960dd77116f7701289b20413f6242a
Guðrún Margrét Ívansdóttir <gudruni17@ru.is>
2019-12-03 18:40:56 +0000

    just testing to see what happens now in the console output

--- scripts/initialize_game_api_instance.sh
+++ scripts/initialize_game_api_instance.sh
@@ -14,7 +14,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce 
 sudo usermod -aG docker ubuntu
 
 echo 'Install Docker Compose'
