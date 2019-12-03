4e362e85a53fb4f514ffb1b4cf8b7ec155c95b0e
hjortur <hjorturjohann94@gmail.com>
2019-12-03 16:14:31 +0000

    docker build and push scripts now exit if any command in those scripts fails

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+#set -e exits if any command fails
+set -e
+
 #Get first argument of executable!
 GIT_COMMIT=$1
 
 docker push hjorturj/hgop:$GIT_COMMIT
-
-# TODO exit on error if any command fails
