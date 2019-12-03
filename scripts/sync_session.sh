82045b7b500816abcd153158c0ca04f98902ccc2
hjortur <hjorturjohann94@gmail.com>
2019-12-03 16:51:45 +0000

    The jenkins url was wrong again, changed it to the public one from the aws ec2 instance online

--- scripts/sync_session.sh
+++ scripts/sync_session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JENKINS_URL=http://ec2-52-54-102-207.compute-1.amazonaws.com
+JENKINS_URL=ec2-52-54-102-207.compute-1.amazonaws.com
 
 scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
 ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
