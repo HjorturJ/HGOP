55a7c6172ece02c215edc1881567b7bc460c2b1c
Guðrún Margrét Ívansdóttir <gudruni17@ru.is>
2019-12-03 15:54:31 +0000

    rules added in eslint and fixed lint in database

--- game_api/database.js
+++ game_api/database.js
@@ -3,12 +3,12 @@ const { Client } = require('pg');
 // export available database functions.
 module.exports = {
     insertItem: (name, insertDate, onInsert) => {
-        var client = getClient();
+        let client = getClient();
         client.connect(() => {
             const query = {
                 text: 'INSERT INTO Item(Name, InsertDate) VALUES($1, $2);',
                 values: [name, insertDate],
-            }
+            };
             client.query(query, () => {
                 onInsert();
                 client.end();
@@ -17,12 +17,12 @@ module.exports = {
         return;
     },
     getItems: (onGet) => {
-        var client = getClient();
+        let client = getClient();
         client.connect(() => {
             const query = {
                 text: 'SELECT ID, Name, InsertDate FROM Item ORDER BY InsertDate DESC LIMIT 10;',
                 rowMode: 'array'
-            }
+            };
             client.query(query, (err, res) => {
                 onGet(res.rows.map(row => {
                     return {
@@ -49,17 +49,17 @@ function getClient() {
 
 // give the database a few seconds to setup before we connect to it.
 setTimeout(() => {
-    var client = getClient();
+    let client = getClient();
     client.connect((err) => {
-        if (err) {
+        if(err) {
             console.log('failed to connect to postgres!');
         } else {
             console.log('successfully connected to postgres!');
             client.query('CREATE TABLE IF NOT EXISTS Item (ID SERIAL PRIMARY KEY, Name VARCHAR(32) NOT NULL, InsertDate TIMESTAMP NOT NULL);', (err) => {
-                if (err) {
-                    console.log('error creating Item table!')
+                if(err) {
+                    console.log('error creating Item table!');
                 } else {
-                    console.log('successfully created item table!')
+                    console.log('successfully created item table!');
                 }
                 client.end();
             });
