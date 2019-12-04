a98b005a7e32494540f1a39a7195064e19a6dbeb
hjortur <hjorturjohann94@gmail.com>
2019-12-04 17:59:57 +0000

    Added dealer tests, all required tests done, could add more if we think of test cases

--- game_api/dealer.unit-test.js
+++ game_api/dealer.unit-test.js
@@ -7,7 +7,28 @@ function newRandom(randomReturnValues) {
     };
 }
 
-test('dealer should should shuffle cards', () => {
+// Shuffle test
+test('dealer should not shuffle cards', () => {
+    // Arrange
+    let dependencies = {
+        'random': () => newRandom([2, 1]),
+    };
+    let newDealer = require('./dealer.js');
+    let dealer = newDealer((name) => {
+        return dependencies[name];
+    });
+    let deck = ['a', 'b', 'c'];
+    dealer.shuffle = (deck) => {};
+
+    // Act
+    dealer.shuffle(deck);
+
+    // Assert
+    expect(deck).toEqual(['a', 'b', 'c']);
+});
+
+// Shuffle test
+test('dealer should shuffle cards in a non-random way', () => {
     // Arrange
     let dependencies = {
         'random': () => newRandom([2, 1]),
@@ -23,4 +44,49 @@ test('dealer should should shuffle cards', () => {
 
     // Assert
     expect(deck).toEqual(['c', 'b', 'a']);
+});
+
+// Draw test
+test('dealer should return expected card after drawing', () => {
+    // Arrange
+    let dependencies = {
+        'random': () => newRandom([2, 1]),
+    };
+    let newDealer = require('./dealer.js');
+    let dealer = newDealer((name) => {
+        return dependencies[name];
+    });
+    let deck = ['a', 'b', 'c'];
+
+    // Act
+    dealer.shuffle(deck);
+    let card = dealer.draw(deck);
+
+    // Assert
+    expect(card).toEqual('a');
+});
+
+// Draw test
+test('dealer should return expected order of cards and be empty after 3 draws', () => {
+    // Arrange
+    let dependencies = {
+        'random': () => newRandom([2, 1]),
+    };
+    let newDealer = require('./dealer.js');
+    let dealer = newDealer((name) => {
+        return dependencies[name];
+    });
+    let deck = ['a', 'b', 'c'];
+
+    // Act
+    dealer.shuffle(deck);
+    let card01 = dealer.draw(deck);
+    let card02 = dealer.draw(deck);
+    let card03 = dealer.draw(deck);
+
+    // Assert
+    expect(card01).toEqual('a');
+    expect(card02).toEqual('b');
+    expect(card03).toEqual('c');
+    expect(deck).toEqual([]);
 });
