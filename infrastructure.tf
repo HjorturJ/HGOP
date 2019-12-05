9c2e1ac584c860c1182e949cb9b6aa0a1a1f801f
hjortur <hjorturjohann94@gmail.com>
2019-12-05 13:53:46 +0000

    Undoing infrastructure.tf changes to see if that gets us a valid terraform setup again

--- infrastructure.tf
+++ infrastructure.tf
@@ -109,6 +109,22 @@ resource "aws_instance" "game_server" {
     }
   }
 
+  # Executing the chmod +x in our server on the local 
+  # initialize script to make it executable
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/docker_compose_up.sh",
+    ]
+
+    connection {
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.aws/GameKeyPair.pem")
+    }
+  }
+}
+
 # Returning our server public ip from this script
 output "public_ip" {
   value = aws_instance.game_server.public_ip
