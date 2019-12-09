0cfa90c329278b9af4f38f9e084a36bdfbdc29f8
hjortur <hjorturjohann94@gmail.com>
2019-12-09 16:30:54 +0000

    if we have count(*) in the sql command, we need to get rows[0] value for the actual count, not rowcount, that would always give us 1 with the count(*) command

--- game_api/database.js
+++ game_api/database.js
@@ -70,7 +70,7 @@ module.exports = function(context) {
                         if (err) {
                             onError(err);
                         } else {
-                            onSuccess(res.rowCount);
+                            onSuccess(res.rows[0]);
                         }
                         client.end();
                     });
