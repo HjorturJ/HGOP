75bd89553ce9fb6499eb2831397b183410f29e82
Guðrún Margrét Ívansdóttir <gudruni17@ru.is>
2019-12-10 22:31:02 +0000

    fix in start_local.sh

--- scripts/start_local.sh
+++ scripts/start_local.sh
@@ -3,7 +3,8 @@
 set -euxo pipefail
 
 docker build game_api -t hjorturj/game_api:dev
-(cd game_client && npm build)
+(cd game_client && npm run build)
 docker build game_client -t hjorturj/game_client:dev
 
 API_URL=localhost GIT_COMMIT=dev docker-compose up
+
