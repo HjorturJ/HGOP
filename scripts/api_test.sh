fb729118a61be5005042b0599236f614cc3e97b1
hjortur <hjorturjohann94@gmail.com>
2019-12-10 14:24:38 +0000

    Getting cant open error on jenkins_deploy despite it being runnable...

--- scripts/api_test.sh
+++ scripts/api_test.sh
@@ -4,5 +4,7 @@ GIT_COMMIT=$1
 
 sh "./scripts/jenkins_deploy.sh ${GIT_COMMIT} apitest"
 API_URL=$(cd /var/lib/jenkins/terraform/hgop/apitest && terraform output public_ip)
+echo $PWD
 cd game_api
+echo $PWD
 sh "API_URL=${API_URL}:3000 npm run test:api"
