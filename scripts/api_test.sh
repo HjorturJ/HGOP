87d7d4e0050a82432f9aa067b61867e4ae89e176
hjortur <hjorturjohann94@gmail.com>
2019-12-10 14:26:29 +0000

    According to stackoverflow, we shouldnt have quotes inside a bash script sh command

--- scripts/api_test.sh
+++ scripts/api_test.sh
@@ -2,9 +2,9 @@
 
 GIT_COMMIT=$1
 
-sh "./scripts/jenkins_deploy.sh ${GIT_COMMIT} apitest"
+sh ./scripts/jenkins_deploy.sh ${GIT_COMMIT} apitest
 API_URL=$(cd /var/lib/jenkins/terraform/hgop/apitest && terraform output public_ip)
 echo $PWD
 cd game_api
 echo $PWD
-sh "API_URL=${API_URL}:3000 npm run test:api"
+sh API_URL=${API_URL}:3000 npm run test:api
