b392e8b828cdf18a539f720a1dc4d1acb8c5362c
hjortur <hjorturjohann94@gmail.com>
2019-12-10 13:19:14 +0000

    Added quotes to currenv variable reference as per stackoverflow suggestion

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -9,25 +9,25 @@ CURR_ENV=$2
 echo "${CURR_ENV}"
 
 # We need to move some files around, because of the terraform state limitations.
-mkdir -p /var/lib/jenkins/terraform/hgop/${CURR_ENV}
-mkdir -p /var/lib/jenkins/terraform/hgop/${CURR_ENV}/scripts
-rm -f /var/lib/jenkins/terraform/hgop/${CURR_ENV}/scripts/initialize_game_api_instance.sh
-cp scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/${CURR_ENV}/scripts/initialize_game_api_instance.sh
-rm -f /var/lib/jenkins/terraform/hgop/${CURR_ENV}/scripts/docker_compose_up.sh
-cp scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/${CURR_ENV}/scripts/docker_compose_up.sh
-rm -f /var/lib/jenkins/terraform/hgop/${CURR_ENV}/docker-compose.yml
-cp docker-compose.yml /var/lib/jenkins/terraform/hgop/${CURR_ENV}/docker-compose.yml
+mkdir -p /var/lib/jenkins/terraform/hgop/""${CURR_ENV}""
+mkdir -p /var/lib/jenkins/terraform/hgop/"${CURR_ENV}"/scripts
+rm -f /var/lib/jenkins/terraform/hgop/"${CURR_ENV}"/scripts/initialize_game_api_instance.sh
+cp scripts/initialize_game_api_instance.sh /var/lib/jenkins/terraform/hgop/"${CURR_ENV}"/scripts/initialize_game_api_instance.sh
+rm -f /var/lib/jenkins/terraform/hgop/"${CURR_ENV}"/scripts/docker_compose_up.sh
+cp scripts/docker_compose_up.sh /var/lib/jenkins/terraform/hgop/"${CURR_ENV}"/scripts/docker_compose_up.sh
+rm -f /var/lib/jenkins/terraform/hgop/"${CURR_ENV}"/docker-compose.yml
+cp docker-compose.yml /var/lib/jenkins/terraform/hgop/"${CURR_ENV}"/docker-compose.yml
 
 # Delete all .tf files from /var/lib/jenkins/terraform/hgop/production
-rm -f /var/lib/jenkins/terraform/hgop/${CURR_ENV}/*.tf
+rm -f /var/lib/jenkins/terraform/hgop/"${CURR_ENV}"/*.tf
 
 # Copy all .tf files from repository to /var/lib/jenkins/terraform/hgop/production
-cp *.tf /var/lib/jenkins/terraform/hgop/${CURR_ENV}/
+cp *.tf /var/lib/jenkins/terraform/hgop/"${CURR_ENV}"/
 
-cd /var/lib/jenkins/terraform/hgop/${CURR_ENV}
+cd /var/lib/jenkins/terraform/hgop/"${CURR_ENV}"
 terraform init # In case terraform is not initialized.
-terraform destroy -auto-approve -var environment=${CURR_ENV} || exit 1
-terraform apply -auto-approve -var environment=${CURR_ENV} || exit 1
+terraform destroy -auto-approve -var environment="${CURR_ENV}" || exit 1
+terraform apply -auto-approve -var environment="${CURR_ENV}" || exit 1
 
 echo $(terraform output public_ip)
 
